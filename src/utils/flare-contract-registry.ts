df02ae3c23bf8a0cc190b8744eab41fb8b59db71
Nik Erzetic <nik.erzetic@aflabs.com>
2026-01-05 14:22:11 +0100

    refactor: use Flare Wagmi periphery package instead of hardcoding

--- src/utils/flare-contract-registry.ts
+++ src/utils/flare-contract-registry.ts
@@ -1,16 +1,15 @@
-import type { Address } from "viem";
-import { abi } from "../abis/IFlareContractRegistry";
 import { publicClient } from "./client";
+import { coston2 } from "test-periphery-artifacts-wagmi-types";
 
 const FLARE_CONTRACT_REGISTRY_ADDRESS = "0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019";
 
 export async function getContractAddressByName(name: string) {
   const contractAddress = await publicClient.readContract({
     address: FLARE_CONTRACT_REGISTRY_ADDRESS,
-    abi: abi,
+    abi: coston2.iFlareContractRegistryAbi,
     functionName: "getContractAddressByName",
     args: [name],
   });
 
-  return contractAddress as Address;
+  return contractAddress;
 }
