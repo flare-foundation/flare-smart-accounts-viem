29351907622facbc7682a4adee0cf64fdb3a0e13
Nik Erzetic <nik.erzetic@aflabs.com>
2026-04-22 11:21:19 +0200

    feat: add tag owner script

---
+++ src/tag-owner.ts
@@ -0,0 +1,29 @@
+import type { Address } from "viem";
+import { publicClient } from "./utils/client";
+import { getContractAddressByName } from "./utils/flare-contract-registry";
+import { getMintingTagManagerAddress } from "./utils/direct-minting";
+import { abi as iMintingTagManagerAbi } from "./abis/IMintingTagManager";
+
+const TAG: bigint = 42n;
+
+async function main() {
+  const assetManagerAddress = await getContractAddressByName("AssetManagerFXRP");
+  const mintingTagManagerAddress = await getMintingTagManagerAddress(assetManagerAddress);
+  console.log("MintingTagManager address:", mintingTagManagerAddress, "\n");
+
+  const owner = (await publicClient.readContract({
+    address: mintingTagManagerAddress,
+    abi: iMintingTagManagerAbi,
+    functionName: "ownerOf",
+    args: [TAG],
+  })) as Address;
+
+  console.log("Owner of tag", TAG.toString() + ":", owner, "\n");
+}
+
+void main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
